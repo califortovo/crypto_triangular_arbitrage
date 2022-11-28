62533ab849e950ac0911286378deaf1392bd5003
Admin <a.spiridonov@age-golden.ru>
2022-11-29 01:06:04 +0500

    push

---
+++ parser.py
@@ -0,0 +1,6 @@
+import ccxt
+
+exchange = ccxt.wazirx({
+    "apiKey": "xakFgqYBzS6qGmJHTj0OdkVbbMOhJvPpR27G7oqMgd58j91WHxsiSSpKCks5XqEY",
+    "secret": "knTqNJLaLLkVRaBC6m9Ikg3dvLqCCZgMayBqe6WKkU8cKbOr6ASrfF6cXNwqStTd"
+})
